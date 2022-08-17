fcc9e8b579325fead319de2e99bbb6993b005e72
sachit393 <sachitsachdeva27@hotmail.com>
2022-08-17 16:48:07 +0530

    stopwatch project added

---
+++ stopwatch/stopwatch.srcs/sources_1/new/stopwatch.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.08.2022 16:45:19
+-- Design Name: 
+-- Module Name: stopwatch - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity stopwatch is
+--  Port ( );
+end stopwatch;
+
+architecture Behavioral of stopwatch is
+
+begin
+
+
+end Behavioral;
